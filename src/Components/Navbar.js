30268d68ba59b9bded7d2b479a9c2840b2f14839
Averek <singhavanish2003@gmail.com>
2021-10-04 09:10:18 +0530

    add-blink

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import {Link, useLocation} from "react-router-dom"
 
 export default function Navbar(props) {
+    let location = useLocation();
     return (
         <>
             <div>
@@ -14,39 +15,28 @@ export default function Navbar(props) {
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                                    <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to="/">Home</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link active" to="/general">General</Link>
+                                    <Link className={`nav-link ${location.pathname === "/general" ? "active" : ""}`} to="/general">General</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link active" to="/entertainment">Entertainmnet</Link>
+                                    <Link className={`nav-link ${location.pathname === "/entertainment" ? "active" : ""}`} to="/entertainment">Entertainmnet</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link active" to="/sports">Sports</Link>
+                                    <Link className={`nav-link ${location.pathname === "/sports" ? "active" : ""}`} to="/sports">Sports</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link active" to="/health">Health</Link>
+                                    <Link className={`nav-link ${location.pathname === "/health" ? "active" : ""}`} to="/health">Health</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link active" to="/business">Business</Link>
+                                    <Link className={`nav-link ${location.pathname === "/business" ? "active" : ""}`} to="/business">Business</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link active" to="/science">Science</Link>
+                                    <Link className={`nav-link ${location.pathname === "/science" ? "active" : ""}`} to="/science">Science</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link active" to="/technology">Technology</Link>
-                                </li>
-                                <li className="nav-item dropdown">
-                                    <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Dropdown
-                                    </Link>
-                                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><Link className="dropdown-item" to="/">Action</Link></li>
-                                        <li><Link className="dropdown-item" to="/">Another action</Link></li>
-                                        <li><hr className="dropdown-divider" /></li>
-                                        <li><Link className="dropdown-item" to="/">Something else here</Link></li>
-                                    </ul>
+                                    <Link className={`nav-link ${location.pathname === "/technology" ? "active" : ""}`} to="/technology">Technology</Link>
                                 </li>
                             </ul>
                             <form className="d-flex">
